0d6f0d39f1b187b191e48cc36599503a1c20f8ee
Evgeniy Kozlov <100347774+evgeniy-kozlov-web@users.noreply.github.com>
2022-02-26 11:36:47 +0200

    Change methods names to correct

--- src/routing/Router.php
+++ src/routing/Router.php
@@ -16,10 +16,10 @@ class Router
 		return $this->routes[$slug] ?? false;
 	}
 
-	public function getRouteByRule(string $path, string $method): array | bool
+	public function getRouteAndMatchesByRule(string $path, string $method): array | bool
 	{
 		$method = strtoupper($method);
-		
+
 		foreach ($this->routes as $route) {
 			if (!empty(preg_match($route->getPath(), $path, $matches)) && $route->getMethod() == $method) return ['route' => $route, 'matches' => array_unique($matches)];
 		}
@@ -27,16 +27,16 @@ class Router
 		return false;
 	}
 
-	public function getCurrentRoute(): array | bool
+	public function getCurrentRouteAndMatches(): array | bool
 	{
-		return $this->getRouteByRule($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+		return $this->getRouteAndMatchesByRule($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
 	}
 
 	public function addRoute(Route $route): Router
 	{
 		if (array_key_exists($route->getSlug(), $this->routes)) throw new \app\exceptions\SlugIsAlreadyExistsException();
 
-		if ($this->getRouteByRule($route->getPath(), $route->getMethod())) throw new \app\exceptions\RuleIsAlreadyExistsException();
+		if ($this->getRouteAndMatchesByRule($route->getPath(), $route->getMethod())) throw new \app\exceptions\RuleIsAlreadyExistsException();
 
 		$this->routes[$route->getSlug()] = $route;
 
